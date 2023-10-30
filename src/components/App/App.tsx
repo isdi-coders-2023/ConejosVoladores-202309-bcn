8332d90ac1c734a49edaf9cd05ab6d372692f22b
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-10-30 19:02:12 +0100

    Add router and provider

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
 const App = (): React.ReactElement => {
-  return <main className="main-content"></main>;
+  return (
+    <>
+      <main className="main-content>">
+        <Routes>
+          <Route path="/home" />
+        </Routes>
+      </main>
+    </>
+  );
 };
 
 export default App;
+<main className="main-content"></main>;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
