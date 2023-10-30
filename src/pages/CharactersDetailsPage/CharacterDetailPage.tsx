fbe75387c2b9a33ebccb21db629a53481dd104d9
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-10-30 20:38:19 +0100

    Change naming from CharacterDetailPage to HomePage

--- src/pages/CharactersDetailsPage/CharacterDetailPage.tsx
+++
@@ -1,8 +0,0 @@
-const CharacterDetailsPage = (): React.ReactElement => {
-  return (
-    <>
-      <h2>Characters</h2>
-    </>
-  );
-};
-export default CharacterDetailsPage;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,8 @@
+const HomePage = (): React.ReactElement => {
+  return (
+    <>
+      <h2>Characters</h2>
+    </>
+  );
+};
+export default HomePage;
