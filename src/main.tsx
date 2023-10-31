c72002c8fdca88aa5259f2b3ff62f84669c96e6f
Anna Roses <anna.roses.marti@gmail.com>
2023-10-31 10:29:00 +0100

    Merge btanch 'main' into feature/MainHeader-component

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./styles/mainTheme";
+import GlobalStyle from "./styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
-  </React.StrictMode>
+    <ThemeProvider theme={mainTheme}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>,
 );
