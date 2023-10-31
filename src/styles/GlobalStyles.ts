6cefb38e6d7cd61baf35e86a7b114cfd6b634517
Matias Risco <matiasriscolagos@gmail.com>
2023-10-31 13:54:05 +0100

    Fix naming of interface DefaultTheme

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -29,10 +29,10 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: ${({ theme }) => theme.typography["main-font-family"]};
-    color: ${({ theme }) => theme.color["main-font"]};
-    background-color: ${({ theme }) => theme.color["main-background"]};
-    font-size: ${({ theme }) => theme.typography["properties-size"]};
+    font-family: ${({ theme }) => theme.typography["mainFontFamily"]};
+    color: ${({ theme }) => theme.color["mainFont"]};
+    background-color: ${({ theme }) => theme.color["mainBackground"]};
+    font-size: ${({ theme }) => theme.typography["propertiesSize"]};
   }
 
 `;
