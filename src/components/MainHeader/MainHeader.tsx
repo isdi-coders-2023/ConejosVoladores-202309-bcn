c026064cbef0e1b7e85f621f0b2bfcd8cceb1e6d
Anna Roses <anna.roses.marti@gmail.com>
2023-10-31 12:38:42 +0100

    Delete extra header in MainHeader

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -2,15 +2,13 @@ import MainHeaderStyled from "./MainHeaderStyled";
 
 const MainHeader = (): React.ReactElement => {
   return (
-    <MainHeaderStyled>
-      <header className="header">
-        <img
-          src="/favicon.webp"
-          alt="Super Smash Bross logo"
-          className="header__logo"
-        />
-        <span className="header__title">Mario Smashers</span>
-      </header>
+    <MainHeaderStyled className="header">
+      <img
+        src="/favicon.webp"
+        alt="Super Smash Bross logo"
+        className="header__logo"
+      />
+      <span className="header__title">Mario Smashers</span>
     </MainHeaderStyled>
   );
 };
