e803b52bd06c8a65a2dabdbdbd005369dd5506fa
Matias Risco <matiasriscolagos@gmail.com>
2023-10-31 13:56:04 +0100

    Fix naming of variable GlobalStyle

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
   @import url("https://fonts.cdnfonts.com/css/new-super-mario-font-u");
 
   *,
@@ -37,4 +37,4 @@ const GlobalStyle = createGlobalStyle`
 
 `;
 
-export default GlobalStyle;
+export default GlobalStyles;
