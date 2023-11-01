217e80e906bde707b5dceee4634532abbe6087c3
Matias Risco <matiasriscolagos@gmail.com>
2023-11-01 16:40:20 +0100

    Fix naming propeties in HomePageStyled

--- src/pages/HomePage/HomePageStyled.ts
+++ src/pages/HomePage/HomePageStyled.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 const HomePageStyled = styled.section`
   display: flex;
   justify-content: space-around;
-  font-family: ${({ theme }) => theme.typography["secondaryFontFamily"]};
-  font-size: ${({ theme }) => theme.typography["titleSize"]};
-  color: ${({ theme }) => theme.color["mainFont"]};
+  font-family: ${({ theme }) => theme.typography.secondaryFontFamily};
+  font-size: ${({ theme }) => theme.typography.titleSize};
+  color: ${({ theme }) => theme.color.mainFont};
 `;
 
 export default HomePageStyled;
