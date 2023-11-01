b97a7ea75a56b19faf54b1aec52b00bce9cc075a
Anna Roses <anna.roses.marti@gmail.com>
2023-11-01 16:20:36 +0100

    Call to loadCharacters in HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,23 @@
+import { useContext, useEffect } from "react";
+import mockCharacters from "../../data/mockData";
+import CharactersContext from "../../features/characters/strore/CharactersContext";
+import characterStructureApiToCharacterStructure from "../../data/apiSmash";
+import { CharacterStructure } from "../../features/characters/types";
+
 const HomePage = (): React.ReactElement => {
+  const structuredMockCharacters = mockCharacters.map(
+    (character): CharacterStructure => {
+      return characterStructureApiToCharacterStructure(character);
+    },
+  );
+  const { loadCharacters } = useContext(CharactersContext);
+
+  useEffect(
+    () => loadCharacters(structuredMockCharacters),
+    [structuredMockCharacters, loadCharacters],
+  );
+
   return <h2>Characters</h2>;
 };
+
 export default HomePage;
