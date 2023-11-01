d528d63ca6d7a3b43c065a50398057dc79ad9469
Matias Risco <matiasriscolagos@gmail.com>
2023-11-01 08:44:46 +0100

    Add CharacterList to page HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,11 @@
+import CharacterList from "../../components/CharacterList/CharacterList";
+
 const HomePage = (): React.ReactElement => {
-  return <h2>Characters</h2>;
+  return (
+    <>
+      <h2>Characters</h2>
+      <CharacterList />
+    </>
+  );
 };
 export default HomePage;
