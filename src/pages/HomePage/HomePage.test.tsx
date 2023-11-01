5089b050df4a85678b9d595dfbcb0257e45789b7
Anna Roses <anna.roses.marti@gmail.com>
2023-11-01 17:29:26 +0100

    Add CharacterProviderWrapper in test

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -2,16 +2,19 @@ import { ThemeProvider } from "styled-components";
 import HomePage from "./HomePage";
 import { screen, render } from "@testing-library/react";
 import mainTheme from "../../styles/mainTheme";
+import CharacterProviderWrapper from "../../features/characters/strore/CharactersWrapper";
 
 describe("Given a HomePage component", () => {
-  describe("When it is call", () => {
-    test("Then it should show heading with text Character", () => {
+  describe("When it is called", () => {
+    test("Then it should show a heading with the text Characters", () => {
       const expectedText = "Characters";
 
       render(
-        <ThemeProvider theme={mainTheme}>
-          <HomePage />
-        </ThemeProvider>,
+        <CharacterProviderWrapper>
+          <ThemeProvider theme={mainTheme}>
+            <HomePage />
+          </ThemeProvider>
+        </CharacterProviderWrapper>,
       );
 
       const text = screen.getByText(expectedText);
