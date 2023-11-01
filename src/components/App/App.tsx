b15fb47b68a905836db6034b91879436c594493a
Matias Risco <matiasriscolagos@gmail.com>
2023-11-01 08:55:08 +0100

    Add Route '/' to navigate to '/home' and implemented AppStyles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
+import HomePage from "../../pages/HomePage/HomePage";
+import MainStyled from "./AppStyles";
 
 const App = (): React.ReactElement => {
   return (
     <>
       <Header />
-      <main className="main-content>">
+      <MainStyled className="main-content>">
         <Routes>
-          <Route path="/home" />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
         </Routes>
-      </main>
+      </MainStyled>
     </>
   );
 };
