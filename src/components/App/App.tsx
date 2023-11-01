5f47b99044c9e618047458e9ece4580083608832
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-01 16:29:48 +0100

    Add routes for the homepage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,15 @@
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "../../pages/HomePage/HomePage";
+
 const App = (): React.ReactElement => {
-  return <main className="main-content"></main>;
+  return (
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
