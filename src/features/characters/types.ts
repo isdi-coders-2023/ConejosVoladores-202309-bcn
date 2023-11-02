4d93dc3586d88ed167ac68979e528071458810fb
Anna Roses <anna.roses.marti@gmail.com>
2023-11-02 17:44:09 +0100

    Add CharacterStructureApi to types

--- src/features/characters/types.ts
+++ src/features/characters/types.ts
@@ -7,3 +7,21 @@ export interface CharacterStructure {
   smashtype: string;
   imageUrl: string;
 }
+export interface CharacterStructureApi {
+  id: number;
+  name: string;
+  availability: string;
+  alsoAppearsIn: string[];
+  smash: {
+    attack: string;
+    type: string;
+  };
+  images: {
+    icon: string;
+    portrait: string;
+  };
+  series: {
+    icon: string;
+    name: string;
+  };
+}
