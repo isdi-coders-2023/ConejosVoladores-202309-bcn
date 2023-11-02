3f9978377d57ae4f045503d889bfd87cb564a505
Matias Risco <matiasriscolagos@gmail.com>
2023-11-02 09:03:02 +0100

    Add CharacterCardStyle in CharacterCard

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,19 +1,28 @@
 import { CharacterStructure } from "../../features/characters/types";
+import CharacterCardContainer from "./CharacterCardStyle";
 
 interface CharacterCardProps {
   character: CharacterStructure;
 }
 
 const CharacterCard = ({
-  character: { availability, imageUrl, name, smashtype },
+  character: { availability, imageUrl, name, attack },
 }: CharacterCardProps): React.ReactElement => {
   return (
-    <div className="card-conteiner">
-      <img src={imageUrl} alt={`imagen de ${name}`} />
-      <h3>{name}</h3>
-      <span>{availability}</span>
-      <span>{smashtype}</span>
-    </div>
+    <CharacterCardContainer>
+      <img
+        className="character-card__image"
+        src={imageUrl}
+        alt={`imagen de ${name}`}
+        width="110"
+        height="110"
+      />
+      <div className="character-card__property">
+        <h3>{name}</h3>
+        <span>{availability}</span>
+        <span>{attack}</span>
+      </div>
+    </CharacterCardContainer>
   );
 };
 export default CharacterCard;
---
+++ src/components/CharacterCard/CharacterCardStyle.ts
@@ -0,0 +1,33 @@
+import { styled } from "styled-components";
+
+const CharacterCardContainer = styled.div`
+  background-color: ${({ theme }) => theme.color.containerBackground};
+  padding: 22px;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+  border-radius: 24px;
+  width: calc(100vw - 44px);
+  max-width: 316px;
+  height: 339px;
+
+  .character-card {
+    &__image {
+      width: 110px;
+      height: 110px;
+      align-self: center;
+    }
+    &__property {
+      display: flex;
+      flex-direction: column;
+      justify-content: start;
+      padding: 0 14px;
+
+      & * {
+        padding: 5px 0;
+      }
+    }
+  }
+`;
+
+export default CharacterCardContainer;
