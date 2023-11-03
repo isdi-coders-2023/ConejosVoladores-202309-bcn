c0c121eb0ca2baf7343df211adf35b76bc3888ed
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-03 10:51:41 +0100

    Delate useless div with classes

--- src/components/Loading/Loading.test.tsx
+++ src/components/Loading/Loading.test.tsx
@@ -1,15 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import Loading from "./Loading";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "../../styles/mainTheme";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Given a Loading component", () => {
-  describe("When it receives an image with the alt property 'logo loading page'", () => {
-    test("Then it should show a circle image", () => {
-      render(<Loading />);
+  describe("When is render", () => {
+    test("Then it should show `logo loading page`", () => {
+      const expectedAltText = "logo loading page";
+      const expectedImageSrc = "public/images/logo.png";
 
-      const expectedText = "logo loading page";
-      const loadingPage = screen.getByAltText(expectedText);
+      render(
+        <BrowserRouter>
+          <ThemeProvider theme={mainTheme}>
+            <Loading />
+          </ThemeProvider>{" "}
+        </BrowserRouter>,
+      );
 
-      expect(loadingPage).toBeInTheDocument();
+      const loadImage = screen.getByAltText(expectedAltText);
+      expect(loadImage).toHaveAttribute("src", expectedImageSrc);
     });
   });
 });
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,15 +2,6 @@ const Loading = (): React.ReactElement => {
   return (
     <div>
       <img src="public/images/logo.png" alt="logo loading page"></img>
-      <div className="load">
-        <div className="smash-logo">
-          <div className="smash-logo__line-vertical"></div>
-          <div className="logo__line--horizontal"></div>
-        </div>
-        <div className="logo__ld">
-          <div></div>
-        </div>
-      </div>
     </div>
   );
 };
