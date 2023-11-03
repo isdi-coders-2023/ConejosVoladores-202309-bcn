e572b69e79cc5052a5d3a4f20310e3c172d4bc36
Anna Roses <anna.roses.marti@gmail.com>
2023-11-03 16:52:02 +0100

    Change main for div and fix styles

--- src/components/App/AppStyled.ts
+++ src/components/App/AppStyled.ts
@@ -1,18 +1,15 @@
 import styled from "styled-components";
 
-const AppStyled = styled.main`
-  width: 100vw;
-  height: 100vh;
+const AppStyled = styled.div`
   display: flex;
+  min-height: 100vh;
   flex-direction: column;
-  justify-content: space-between;
 
   .main-container {
     display: flex;
     flex-direction: column;
     padding: 14px 22px 25px 22px;
     align-items: center;
-    height: 100%;
   }
 `;
 
