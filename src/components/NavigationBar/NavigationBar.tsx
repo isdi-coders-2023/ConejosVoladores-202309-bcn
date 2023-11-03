9064be0f93fc3b52c19e64e4e455120605305ade
Anna Roses <anna.roses.marti@gmail.com>
2023-11-03 21:00:52 +0100

    Add button active and fix classes

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,22 +1,15 @@
 import { NavLink } from "react-router-dom";
 import NavigationBarStyled from "./NavigationBarStyled";
 import React from "react";
-import { useLocation } from "react-router";
 
 const NavigationBar = (): React.ReactElement => {
-  const { pathname } = useLocation();
-
   return (
     <NavigationBarStyled>
       <ul className="navigation">
         <li>
-          <NavLink to="/home">
+          <NavLink aria-label="home" to="/home">
             <img
-              className={
-                pathname === "/home"
-                  ? "navigation__icon--active"
-                  : "navigation__icon"
-              }
+              className={"navigation__icon navigation__icon--circle"}
               src="images/button-home.svg"
               alt="Home button"
               width={48}
@@ -25,13 +18,9 @@ const NavigationBar = (): React.ReactElement => {
           </NavLink>
         </li>
         <li>
-          <NavLink to="/">
+          <NavLink aria-label="add" to="/">
             <img
-              className={
-                pathname === "/addCharacter"
-                  ? "navigation__icon--active"
-                  : "navigation__icon"
-              }
+              className={"navigation__icon navigation__icon--circle"}
               src="images/button-plus.svg"
               alt="Add character button"
               width={48}
--- src/components/NavigationBar/NavigationBarStyled.ts
+++ src/components/NavigationBar/NavigationBarStyled.ts
@@ -4,13 +4,14 @@ const NavigationBarStyled = styled.nav`
   position: sticky;
   bottom: 0;
   border-top: solid;
+  width: 100%;
+  height: 67px;
+  position: fixed;
 
   .navigation {
     display: flex;
     justify-content: space-around;
     align-items: center;
-    width: 100%;
-    height: 67px;
     padding: 10px;
     background-color: ${({ theme }) => theme.color.containerBackground};
   }
@@ -18,12 +19,15 @@ const NavigationBarStyled = styled.nav`
   .navigation__icon {
     height: 48px;
     width: 48px;
+    border-radius: 100%;
+    border: 3px solid ${({ theme }) => theme.color.secondaryFont};
+  }
 
-    &--active {
-      filter: invert(100%);
-      border: 3px solid ${({ theme }) => theme.color.secondaryFont};
-      border-radius: 100%;
-    }
+  .active {
+    filter: invert(100%);
+    border-radius: 100%;
+    border: 3px solid ${({ theme }) => theme.color.secondaryFont};
+    background-color: ${({ theme }) => theme.color.secondaryFont};
   }
 `;
 
